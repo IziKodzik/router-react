92c6213b7acc9eaf8ba3d9b0fbe78fb1584e06e8
itiguuy@gmail.com <itiguuy@gmail.com>
2021-02-24 15:58:14 +0100

    router example

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Home(){
+    return (
+        <div className="home">
+            homo
+        </div>
+    )
+}
+export default Home
---
+++ src/pages/Products.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Products(){
+    return (
+        <div className="home">
+            homos
+        </div>
+    )
+}
+export default Products
